9a7337e144170ce34e23898e5faf6d5d133d17ec
Nadeem Ahmed <142893027+Nadahm3d@users.noreply.github.com>
2025-12-03 12:58:20 +0100

    Første commit: React TypeScript prosjekt

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <p>Dette er mitt TypeScript React-prosjekt</p>
+    </div>
+  )
+}
+
+export default App
